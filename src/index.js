63e78b5271a4602896c7603b93aca1bb048066c9
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-10-16 13:08:54 +0100

    fix aaalll issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 window.onscroll = function() {moveNav()};
+window.onload = function () {checkScrollBar()}
 
 function moveNav() {
     document.getElementById("nav").style.backgroundColor = "rgba(2,2,2,0.7)";
   }
+
+function checkScrollBar() {
+    if (window.scrollY !== 0) {
+        document.getElementById("nav").style.backgroundColor = "rgba(2,2,2,0.7)";
+    }
+}
 
 function toogleBurger() {
     const element = document.getElementById("navbar");
